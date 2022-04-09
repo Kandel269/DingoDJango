7c87985b1dc4743ba0b01e0dad8c04412ab7b212
Uzytkownik <uzytkownik@kursGIT.pl>
2022-04-09 14:46:31 +0100

    uuids_a in progress

--- books/views.py
+++ books/views.py
@@ -1,4 +1,9 @@
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def get_hello(request: WSGIRequest) -> HttpResponse:
+    return HttpResponse("<h1>hello world</h1>")
 
+def get_uuids_a(request: WSGIRequest) -> HttpResponse:
+    return HttpResponse("test")
