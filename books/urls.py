fbc7bdb270b8d257fbf51ff7e0e8ddd806bf8c7f
Uzytkownik <uzytkownik@kursGIT.pl>
2022-04-21 10:59:55 +0100

    logging

--- books/urls.py
+++ books/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from books.views import get_uuids_a, get_uuids_b, get_argument_from_path, get_argument_from_query, \
     check_http_query_type, get_headers, raise_error_for_fun, AuthorListBaseView, CategoryListTemplateView, \
-    BooksListView, BooksDetails
+    BooksListView, BooksDetails, CategoryCreateFormsView
 
 urlpatterns = [
     path('uuids-a', get_uuids_a),
@@ -16,4 +16,5 @@ urlpatterns = [
     path('category-list', CategoryListTemplateView.as_view(), name = "category_list"),
     path('books-list', BooksListView.as_view(), name = "books_list"),
     path('book-details/<int:pk>/', BooksDetails.as_view(), name = "books_details"),
+    path('category-create',CategoryCreateFormsView.as_view(), name = "category_create"),
 ]
