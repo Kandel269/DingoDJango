09f99f47ae74478b3a3b5afc80e8582932b19ad7
Uzytkownik <uzytkownik@kursGIT.pl>
2022-05-13 02:03:20 +0100

    added app users

--- books/models.py
+++ books/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+
 from django.contrib.auth.models import User
 from  django.db.models import QuerySet
 from django.db.models import Q
